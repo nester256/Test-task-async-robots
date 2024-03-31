576864aedcccb86a7cfab0bbcd8d72fe37ac9236
nester256 <nestvlad2004@gmail.com>
2024-04-01 00:51:08 +0300

    Delite fixtures because they are not needed

--- scripts/web/startup.sh
+++ scripts/web/startup.sh
@@ -5,7 +5,5 @@ echo "Start service"
 # migrate database
 python scripts/migrate.py
 
-python scripts/load_data.py fixture/main_db/main_db.robot_stop.json
-
 # run app
 exec uvicorn webapp.main:create_app --host=$BIND_IP --port=$BIND_PORT
